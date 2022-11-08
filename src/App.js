7468485ffc5401e3a31be5b16ce256082b302551
Akhila H <Akhilah@er.ai>
2022-11-08 21:51:05 +0530

    testnet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Col, Button, Card, Form } from "react-bootstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <form>
+          <Card>
+            <Form>
+              <Row>
+                <Col md>
+                  <div>
+                    <h2>Blockport Testnet Faucet</h2>
+                  </div>
+                  <Form.Group controlId="Address">
+                    <Form.Control
+                      type="Address"
+                      placeholder="Enter Wallet Address"
+                    />
+                    <Form.Text className="text-muted" />
+                  </Form.Group>
+                </Col>
+
+                <Col md>
+                  <Form.Group controlId="Polygon">
+                    <Form.Control type="Polygon" placeholder="Polygon" />
+                    <Form.Text className="text-muted" />
+                  </Form.Group>
+                </Col>
+
+                <Col md>
+                  <Form.Group controlId="Polygon">
+                    <Button variant="secondary">Fund me</Button>
+                    <Form.Text className="text-muted" />
+                  </Form.Group>
+                </Col>
+              </Row>
+            </Form>
+          </Card>
+        </form>
       </header>
     </div>
   );
